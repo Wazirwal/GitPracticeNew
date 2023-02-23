77afdf38947475a3f7a169f59f7435294fd88701
gitpackage <noor@gmail.com>
2023-02-22 21:49:15 -0500

    Code added to the class

--- src/GitNewPractice/GitClass.java
+++ src/GitNewPractice/GitClass.java
@@ -3,7 +3,7 @@ package GitNewPractice;
 public class GitClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("I am learning gitHub");
 
 	}
 
